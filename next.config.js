86c68ef646fc7594a1bd169d988a7ac7c6dead5e
nemo0 <subhachanda88@gmai.com>
2022-08-17 19:37:32 +0530

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['images.clerk.dev'],
+    domains: ['images.clerk.dev', 'www.gravatar.com'],
   },
 };
 
